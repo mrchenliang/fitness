2dde75bae2c3e4c179e1f3bb0a05acc6601229d4
chen <chen@arternal.com>
2020-09-24 01:49:17 -0400

    Updates

---
+++ precache-manifest.327a81e7d37d7979e6332cc49598318f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac57e1e5da8834bddc10bae1f065d8ac",
+    "url": "/fitness/index.html"
+  },
+  {
+    "revision": "55a5dcda6f9ca235b745",
+    "url": "/fitness/static/css/main.fde26341.chunk.css"
+  },
+  {
+    "revision": "0dcd5f1c02737290b7dc",
+    "url": "/fitness/static/js/2.6152c8fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fitness/static/js/2.6152c8fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55a5dcda6f9ca235b745",
+    "url": "/fitness/static/js/main.ca4de1a7.chunk.js"
+  },
+  {
+    "revision": "adecf5379d8b447e5147",
+    "url": "/fitness/static/js/runtime-main.6ca37e0a.js"
+  },
+  {
+    "revision": "a094a7ad353db364d10526a3563a95c6",
+    "url": "/fitness/static/media/background.a094a7ad.svg"
+  },
+  {
+    "revision": "c596bc6d1abcc98748e717d75f901e84",
+    "url": "/fitness/static/media/icon.c596bc6d.png"
+  }
+]);
