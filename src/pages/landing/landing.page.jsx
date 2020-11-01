2f39d14bc562b4c93aa8c8dadf31ee5bcbeb6e36
Chen Liang <chenliang@Chens-MacBook-Pro.local>
2020-11-01 01:09:19 -0600

    fixing landing page footer

--- src/pages/landing/landing.page.jsx
+++ src/pages/landing/landing.page.jsx
@@ -18,19 +18,20 @@ const Landing = () => {
         </div>
         <div className = 'footer'>
             <footer>
-              <ul className="Social-Icons">
-                <li>
-                  <div>
-                    <a href="mailto:brittany.arora@fitbritfitness.ca" rel="noopener noreferrer" target="_blank"><i className="fa fa-envelope icon footertooltip"><div className = 'footertooltiptext'>Contact</div></i></a>
-                  </div>
-                </li>
-                <li>
-                  <div>
-                    <a href="http://instagram.com/fitbrit_runs" rel="noopener noreferrer" target="_blank"><i className="fa fa-instagram icon footertooltip"><div className = 'footertooltiptext'>Instagram</div></i></a>              
-                  </div>
-                </li>
-              </ul>
-              <p className = 'copyright'>Copyright © FitBrit Fitness {year}</p>
+            <ul className="Social-Icons" id = "contact">
+              <li>
+                <div>
+                  <a href="mailto:brittany.arora@fitbritfitness.ca" rel="noopener noreferrer" target="_blank"><i className="fa fa-envelope icon footertooltip"><div className = 'footertooltiptext'>brittany.arora@fitbritfitness.ca</div></i></a>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <a href="http://instagram.com/fitbrit_runs" rel="noopener noreferrer" target="_blank"><i className="fa fa-instagram icon footertooltip"><div className = 'footertooltiptext'>@fitbrit_runs</div></i></a>              
+                </div>
+              </li>
+            </ul>
+            <p className = 'email'><a href="mailto:brittany.arora@fitbritfitness.ca" rel="noopener noreferrer" target="_blank">brittany.arora@fitbritfitness.ca</a></p>
+            <p className = 'copyright'>Copyright © FitBrit Fitness {year} | Development By <a href="http://chenliang.ca" rel="noopener noreferrer" target="_blank">Chen Liang</a></p>
             </footer>
           </div>
       </div>
