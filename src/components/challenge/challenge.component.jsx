3efe4299521c472c26f70a1920ba3f5ce88a2820
mrchenliang <hi@chenliang.ca>
2023-06-26 08:24:01 -0400

    new challenge

--- src/components/challenge/challenge.component.jsx
+++ src/components/challenge/challenge.component.jsx
@@ -5,7 +5,7 @@ import { AiOutlineClose } from 'react-icons/ai';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import PaypalButtons from '../paypal/paypal.component'
-import { challenge } from '../../types/types';
+import { challenge, challengeCost, challengeDates, challengeDescription } from '../../types/types';
 
 
 class Challenge extends Component {
@@ -37,8 +37,8 @@ class Challenge extends Component {
             return (
                 <div className='program-component' id = "challenge">
                     <div className = 'program-intro'>The FitBrit Fitness 2023 {challenge}!</div>
-                    <div className = 'program-paragraph'>4 Fast Weeks For Fast Progress</div>
-                    <div className = 'program-paragraph'>May 29th, 2023 – June 25th, 2023</div>
+                    <div className = 'program-paragraph'>{challengeDescription}</div>
+                    <div className = 'program-paragraph'>{challengeDates}</div>
                     <div className = 'program-info'>
                         <div className="program-table">
                             <table>
@@ -50,13 +50,24 @@ class Challenge extends Component {
                                 </thead>
                                 <tbody>
                                     <tr>
+                                        <td>All-In-One App <span role = 'img' aria-label = "fitness app">📈</span></td>
+                                        <td>
+                                            <ul className='program-list'>
+                                                <li>Workouts, fuel guide, food logging, progress measuring tools, goal setting, habit tracker, recipe data base, support group, and private personal trainer messaging all in a simple to use app</li>
+                                                <li>Connects with fitness watches to streamline activity/workout syncing</li>
+                                                <li>Positive accountability reinforcement with fun winning streaks, new personal record notifications, and special achievement badges</li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                    <tr>
                                         <td>Weekly Workout Plans <span role = 'img' aria-label = "fitness plan">💪</span></td>
                                         <td>
                                             <ul className='program-list'>
                                                 <li>Home/Gym based workouts in 30 minutes or less</li>
-                                                <li>Exercise modifications for every ability level</li>
-                                                <li>LIVE group workouts</li>
-                                                <li>No equipment necessary workouts</li>
+                                                <li>Just need dumbbells!</li>
+                                                <li>Exercise modifications with beginner to challenging difficulties</li>
+                                                <li>LIVE group workouts (optional)</li>
+                                                <li>No equipment workouts designed for vacations</li>
                                             </ul>
                                         </td>
                                     </tr>
@@ -64,9 +75,29 @@ class Challenge extends Component {
                                         <td>Fuel Plan <span role = 'img' aria-label = "fitness meal">🍔</span></td>
                                         <td>
                                             <ul className='program-list'>
-                                                <li>100% personalized to meet your individual goals</li>
-                                                <li>Flexible fueling program</li>
-                                                <li>Calorie goal, macro targets, and/or meal guide</li>
+                                                <li>100% personalized fueling program to meet your individual goals</li>
+                                                <li>Choice of fuel plan (from lenient to more structured)</li>
+                                                <li>In app food tracking, photo food journal, and product scanning</li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Unlimited Virtual Support <span role = 'img' aria-label = "fitness check">✅</span></td>
+                                        <td>
+                                            <ul className='program-list'>
+                                                <li>Available for 24/7 support, accountability, and motivating</li>
+                                                <li>Available for private video calls to discuss progress / assess form</li>
+                                                <li>Weekly holistic check ins so I can support you best</li>
+                                            </ul>
+                                        </td>                                    
+                                    </tr>
+                                    <tr>
+                                        <td>Prizes! <span role = 'img' aria-label = "fitness prize">🏆</span></td>
+                                        <td>
+                                            <ul className='program-list'>
+                                                <li>Fun prizes to stay motivated</li>
+                                                <li>You vs You, never compared against others</li>
+                                                <li>Holistic transformation considered (mental/emotional progress, overcoming adversity, not giving up, etc. – not just physical progress)</li>
                                             </ul>
                                         </td>
                                     </tr>
@@ -74,29 +105,12 @@ class Challenge extends Component {
                                         <td>Progress Guaranteed <span role = 'img' aria-label = "progress guaranteed">💯</span></td>
                                         <td>
                                             <ul className='program-list'>
-                                                <li>Evidence based programming for science-backed results or your money back</li>
+                                                <li>Evidence based programming for science-backed results</li>
+                                                <li>Achieve your goals with FitBrit as your coach, cheerleader, credible resource, motivator and accountability partner</li>
+                                                <li>Commitment is risk free - results guaranteed or your money back</li>
                                             </ul>
-                                        </td>                                    </tr>
-                                    <tr>
-                                        <td>Unlimited Virtual Support <span role = 'img' aria-label = "fitness check">✅</span></td>
-                                        <td>
-                                            <ul className='program-list'>
-                                                <li>Available for 24/7 support, accountability, and motivating</li>
-                                                <li>Available for private video calls to discuss progress / assess form</li>
-                                            </ul>
-                                        </td>                                    </tr>
-                                    <tr>
-                                        <td>Weekly Progress Check Ins <span role = 'img' aria-label = "fitness check ins">📈</span></td>
-                                        <td>
-                                            <ul className='program-list'>
-                                                <li>Weekly email updates</li>
-                                                <li>Thorough intake to learn how I can best support your journey</li>
-                                            </ul>
-                                        </td>                                       </tr>
-                                    <tr>
-                                        <td>Prizes! <span role = 'img' aria-label = "fitness prize">🏆</span></td>
-                                        <td>3 prize categories + Comeback Ghost option (completely anonymous athlete) + Non-competitive option</td>
-                                    </tr>
+                                        </td> 
+                                   </tr>
                                 </tbody>
                             </table> 
                         </div>
@@ -104,12 +118,12 @@ class Challenge extends Component {
                     <div className = 'program-cards'>
                         <Card className = "program-card" style={{ width: '16rem' }}>
                             <Card.Body>
-                                <Card.Title>FitBrit Fitness ${challenge}</Card.Title>
+                                <Card.Title>FitBrit Fitness {challenge}</Card.Title>
                                 <Card.Text className = "program-price">
-                                CAN $125.00
+                                CAN ${challengeCost}
                                 {/* <span>$89.00</span> */}
                                 </Card.Text>
-                                <Button variant="outline-dark" className = "program-button" onClick={() => this.showPaypalButtons({program: "FitBrit Fitness Summer Challenge", amount: "125.00"})} >Get Started</Button>
+                                <Button variant="outline-dark" className = "program-button" onClick={() => this.showPaypalButtons({program: `FitBrit Fitness ${challenge}`, amount: `${challengeCost}`})} >Get Started</Button>
                             </Card.Body>
                         </Card>
                     </div>
--- src/types/types.js
+++ src/types/types.js
@@ -1,1 +1,4 @@
-export const challenge = 'Sprint Challenge';
+export const challenge = 'Sustain Challenge';
+export const challengeDescription = '8 Weeks For Lasting Change';
+export const challengeDates = 'July 3rd, 2023 – Aug 27th, 2023'
+export const challengeCost = '185.00'
